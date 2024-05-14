55b42bccd8e8ac45e863b1d8fb4323d8e828610a
Joshi2032 <joshi.jc2032@gmail.com>
2024-05-14 10:06:53 -0600

    pai

--- src/app/login-p/login-p.page.ts
+++ src/app/login-p/login-p.page.ts
@@ -2,17 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { logoGoogle, logoFacebook, logoApple } from 'ionicons/icons';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-p',
   templateUrl: './login-p.page.html',
   styleUrls: ['./login-p.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterLink]
 })
 export class LoginPPage implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    addIcons({ logoGoogle, logoFacebook, logoApple  })
+  }
 
   ngOnInit() {
   }
